f0c27fe62a58f2990746ccfe8920229e299fe906
planifyit.com <contact@planifyit.com>
2023-07-28 14:54:04 +0000

    Update GanttChartWidgetAPI.js

--- GanttChartWidgetAPI.js
+++ GanttChartWidgetAPI.js
@@ -63,7 +63,7 @@
         // Convert the task data to the CSV format
         const version = 'public.Actual';  
         const date =  '000000'; 
-        const id = task.id;
+        const id = '1';
         const label = task.text;
         const startDate = task.start_date.toISOString().slice(0,10);  // Convert the date to the format YYYYMMDD
         const endDate = task.end_date.toISOString().slice(0,10);  // Convert the date to the format YYYYMMDD
